bdf2a4e6e77cc185304c2b75087ab26050623181
Italo Alves <aalves_italo@outlook.com>
2026-04-17 14:02:52 -0300

    Refactor Container and HashtagList components; update Footer with copyright notice and styling

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,3 +1,19 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://github.com/Italox7">
+            Italo
+          </a>{" "}
+          . Intended for learning or your portfolio.
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material.
+        </p>
+      </small>
+    </footer>
+  );
 }
