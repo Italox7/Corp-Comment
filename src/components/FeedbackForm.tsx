0287260c8d9b43ad06961a2b55108f6950535512
Italo Alves <aalves_italo@outlook.com>
2026-04-24 11:11:40 -0300

    Implement feedback form with character count and max length validation

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [feedbackText, setFeedbackText] = useState("");
+
+  const charCount = MAX_CHARACTERS - feedbackText.length;
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="text" spellCheck={false} />
+      <textarea
+        id="feedback-textarea"
+        placeholder="text"
+        spellCheck={false}
+        value={feedbackText}
+        onChange={(e) => {
+          setFeedbackText(e.target.value);
+        }}
+        maxLength={MAX_CHARACTERS}
+      />
 
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
 
       <div>
-        <p className="u-italic">150</p>
-        <button>
+        <p className="u-italic">{charCount}</p>
+        <button
+          onClick={() => {
+            console.log("ai meu deus!!");
+          }}
+        >
           <span>Submit</span>
         </button>
       </div>
