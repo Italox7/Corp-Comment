43ac1dd12074c4646948be508a4011515053e433
Italo Alves <aalves_italo@outlook.com>
2026-04-26 20:31:02 -0300

    Fetch feedback data from API and update FeedbackList component

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,34 +1,25 @@
+import { useEffect, useState } from "react";
 import FeedabackItem from "./FeedabackItem";
 
-const feedbackItems = [
-  {
-    upvoteCount: 530,
-    badgeLetter: "I",
-    companyName: "Italo",
-    text: "bla bla bla",
-    daysAgo: 4,
-  },
-  {
-    upvoteCount: 850,
-    badgeLetter: "E",
-    companyName: "Erika",
-    text: "Linda e perfeita",
-    daysAgo: 7,
-  },
-  {
-    upvoteCount: 154,
-    badgeLetter: "U",
-    companyName: "Uiui",
-    text: "bla bla bla",
-    daysAgo: 2,
-  },
-];
-
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFeedbackItems(data.feedbacks);
+      });
+  }, []);
+
   return (
     <ol className="feedback-list">
       {feedbackItems.map((feedbackItem) => (
-        <FeedabackItem feedbackItem={feedbackItem} />
+        <FeedabackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
     </ol>
   );
